61a35038ad81051ded9668596e411d5d76181d6a
rahayon <rahidul_islam@criptext.com>
2021-03-12 12:05:07 +0600

    Updated

--- constructon_project/settings.py
+++ constructon_project/settings.py
@@ -122,6 +122,7 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     BASE_DIR / 'static'
 ]
+STATIC_ROOT = BASE_DIR/'static'
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
 django_heroku.settings(locals())
