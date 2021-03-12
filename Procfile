c5d0bfc7f3c97c9351435b6a445e9bd97fda7556
rahayon <rahidul_islam@criptext.com>
2021-03-12 08:22:24 +0600

    updated

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn construction_project.wsgi
--- constructon_project/settings.py
+++ constructon_project/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
 from pathlib import Path
-
+import django_heroku
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -124,3 +124,4 @@ STATICFILES_DIRS = [
 ]
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
+django_heroku.settings(locals())
